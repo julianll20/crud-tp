6ff4bd12eaac354054ee3284587ca3da8d2dfb6b
julianll22 <julianllera20@gmail.com>
2024-05-28 13:32:28 -0300

    Revert "Primer commit"
    This reverts commit f5e9d3d6f613a76e13b37c463ad2c45278a171ca.

--- src/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { create, get } from "../controllers/userController.js"
-
-const userRoute = express.Router();
-//endpoints
-userRoute.post("/create", create);
-userRouteRoute.get("getAll", get);
-export default userRoute;
-
